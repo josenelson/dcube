d8ffda4538cd4b73e2f62b4222d09e05039f1cc4
Jose Nelson Afonseca Alves <jose.nelson.afonseca@gmail.com>
2016-04-10 20:35:42 -0700

    added docs

--- src/cube.js
+++ src/cube.js
@@ -1,33 +1,72 @@
 import Slice from './slice';
 
+/**
+ * Represents a Cube.
+ * @constructor
+ */
 const DCube = function(){
   let _dimensions = [],
       _facts = [],
       _data = [],
       _rootNode = null;
 
+  /**
+   * Generates a object model of the cube containing:
+   *       Slice1      Slice2      Slice3      ...
+   *      - facts     - facts     - facts
+   *      - values    - values    - values
+   *      - children  - children  - children
+   *
+   * Each children is an array of slices
+   */
   let buildTree = function() {
     if(_dimensions.length == 0) return;
     _rootNode = Slice(_dimensions, _data, _facts);
   }
 
+  /**
+   * Returns the result of the buildTree method.
+   *
+   */
   const ret = function() {
     let result = _rootNode();
     return result;
   }
 
+  /**
+   * Gets or sets an array of dimensions
+   * each dimension can be represented by a string
+   * or a callback function that returns and value
+   * that can be compared using ====
+   *
+   * @param {Array} - the array of dimensions
+   */
   ret.dimensions = function(_) {
     if(arguments.length === 0) return _dimensions;
     _dimensions = _;
     return ret;
   }
 
+  /**
+   * Gets or sets an array of facts
+   * each fact is represented by and object
+   * with the following properties:
+   *  - name: a string representation of the fact, this will map to the facts object on the slice
+   *  - reduce: a callback (f([data])) with will compute the value of the fact given an array of data
+   *  - map: optionally a nap call back to transform the data being passed on the reduce function
+   *
+   * @param {Array} - the array of dimensions
+   */
   ret.facts = function(_) {
     if(arguments.length === 0) return _facts;
     _facts = _;
     return ret;
   }
 
+  /**
+  * The data which the tree will be generated from
+  *
+  */
   ret.data = function(_) {
     if(arguments.length === 0) return _data;
     _data = _;
@@ -37,6 +76,10 @@ const DCube = function(){
     return ret;
   }
 
+  /**
+  * Makes this method a closure.
+  *
+  */
   return ret;
 };
 
--- src/slice.js
+++ src/slice.js
@@ -1,8 +1,14 @@
+/**
+ * Computes the fact values for the data slice
+ *
+ * @param {object} childNode - The current dataslice.
+ * @param {array} data - The data from the facts will be calculated from.
+ * @param {object} facts - Resulting object where the facts will the added.
+ */
 const computeFacts = (childNode, data, facts) => {
   let fact = null,
       childData = childNode.values
       ;
-      //a fact will have, name, reduce, [map]
 
   for(let i = 0; i < facts.length; i++){
     fact = facts[i];
@@ -15,10 +21,18 @@ const computeFacts = (childNode, data, facts) => {
   }
 }
 
+/**
+ * Represents a Slice of data associated with an dimension.
+ * @constructor
+ */
 const Slice = (dimensions, data, facts) => {
   let _groups = [],
       _dimension = dimensions[0];
 
+  /**
+   * Builds the data slice computing the distinct values for the given dimension.
+   *
+   */
   let build = function() {
     let d = null,
         key = null,
@@ -52,6 +66,14 @@ const Slice = (dimensions, data, facts) => {
     }
   }
 
+  /**
+   * Creates the object tree of the data with the following structure.
+   *  - [slice]
+   *    - key: the distinct fact value
+   *    - facts: object with the computer fact values
+   *    - children: array of child slices
+   *    - values: the data associated with this slice
+   */
   let ret = function() {
     /*
       right now it make a copy to the original node,
